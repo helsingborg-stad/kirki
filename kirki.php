7b8e494fae1a86d9208e57274d5fa17bd633a98a
GitHub Action <>
2024-03-19 07:35:00 +0000

    build: update package file versions to 5.1.2

--- kirki.php
+++ kirki.php
@@ -5,7 +5,7 @@
  * Description: The Ultimate WordPress Customizer Framework
  * Author: Themeum
  * Author URI: https://themeum.com
- * Version: 5.1.1
+ * Version: 5.1.2
  * Text Domain: kirki
  * Requires at least: 5.2
  * Requires PHP: 7.1
